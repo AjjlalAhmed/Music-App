e09d41f92e89246594f4783eba7051bd473a9d6b
AjjlalAhmed <ajjlalahmed48@gmail.com>
2020-12-04 17:24:31 +0500

    adding functionality to add songs

--- js/app.js
+++ js/app.js
@@ -1,71 +1,152 @@
 //Adding controls functionality
 (() => {
-  //adding play and pause functionality
   const playBtn = document.querySelector(".play-pause_btn");
   const audio = document.querySelector("audio");
-  audio.volume = 0.3;
+  // Creating dynamic song name
+  const songNameContainer = document.querySelector("#song-name");
+  let marquee = document.createElement("marquee");
+  let audioSrcMarquee = audio.src.match("mixkit-hazy-after-hours-132.mp3");
+  marquee.innerText = audioSrcMarquee[0];
+  marquee.style.textTransform = "uppercase";
+  marquee.style.padding = "10px";
+  marquee.style.color = "#fff";
+  songNameContainer.append(marquee);
   let play = false;
+  function playAndPause() {
+    if (play === false) {
+      audio.play().then(async () => {
+        if (context.state == "suspended") {
+          await context.resume();
+          playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
+          play = true;
+        } else {
+          playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
+          play = true;
+        }
+      });
+    } else {
+      audio.pause();
+      audio.play().then(async () => {
+        if (context.state == "suspended") {
+          await context.resume();
+          playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
+          play = true;
+        } else {
+          playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
+          play = true;
+        }
+      });
+    }
+  }
+  function onload() {
+    let db = new Localbase("db");
+    db.collection("songs")
+      .get()
+      .then((songs) => {
+        songs.forEach((song, index) => {
+          if (index != 0) {
+            songList.push(song.name);
+          }
+        });
+        let screen = document.querySelector("#song-option");
+        let ul = document.createElement("ul"); 
+        songList.forEach((song, index) => {
+          let li = document.createElement("li");
+          li.textContent = song;
+          ul.append(li);
+        });
+        screen.append(ul);
+        ul.addEventListener("click", (e) => {
+          audio.setAttribute("src", `audio/${e.target.innerHTML}`);
+          marquee.textContent = e.target.innerHTML;
+          songs.forEach((song, index) => {
+            if (e.target.innerHTML.includes(song.name)) {
+              audio.setAttribute("src", song.data);
+            }
+          });
+          playAndPause();
+        });
+      });
+  }
+  // Play and pause buttons functionality
   playBtn.addEventListener("click", () => {
     if (play === false) {
       audio.play();
       playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
+      marquee.start();
       play = true;
     } else {
       audio.pause();
       playBtn.innerHTML = `<i class="fa fa-play" aria-hidden="true"></i>`;
+      marquee.stop();
       play = false;
     }
   });
   // adding next and previous funtionality
   const nextBtn = document.querySelector(".next-btn");
   const prevBtn = document.querySelector(".prev-btn");
-  let songSre = [
+  let songList = [
     "mixkit-hazy-after-hours-132.mp3",
     "mixkit-hip-hop-02-738.mp3",
     "mixkit-raising-me-higher-34.mp3",
     "mixkit-tech-house-vibes-130.mp3",
   ];
+  // On load storing song data into song list
+  window.onload = () => {
+    onload();
+  };
+
+  // songList.push(Object.keys(items))
   let songCount = 0;
+  // Next button event
   nextBtn.addEventListener("click", () => {
     nextSong();
   });
   function nextSong() {
     songCount++;
-    if (songCount > 3) songCount = 0;
-    audio.setAttribute("src", `audio/${songSre[songCount]}`);
-    audio.pause();
-    audio
-      .play()
-      .then(() => {
-        playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
-        play = true;
-      })
-      .catch((e) => {
-        //console.log(e);
+    if (songCount > songList.length - 1) songCount = 0;
+    let db = new Localbase("db");
+    db.collection("songs")
+      .get()
+      .then((songs) => {
+        audio.setAttribute("src", `audio/${songList[songCount]}`);
+        songs.forEach((song, index) => {
+          if (song.name.includes(songList[songCount])) {
+            audio.setAttribute("src", song.data);
+          }
+        });
+        songNameContainer.innerHTML = "";
+        marquee.innerText = songList[songCount];
+        songNameContainer.append(marquee);
+        playAndPause();
       });
   }
+  // Prev button event
   prevBtn.addEventListener("click", () => {
     prevSong();
   });
   function prevSong() {
     songCount--;
-    if (songCount < 0) songCount = songSre.length - 1;
-    audio.setAttribute("src", `audio/${songSre[songCount]}`);
-    audio.pause();
-    audio
-      .play()
-      .then(() => {
-        playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
-        play = true;
-      })
-      .catch((e) => {
-        //console.log(e);
+    if (songCount < 0) songCount = songList.length - 2;
+    let db = new Localbase("db");
+    db.collection("songs")
+      .get()
+      .then((songs) => {
+        audio.setAttribute("src", `audio/${songList[songCount]}`);
+        songs.forEach((song, index) => {
+          if (song.name.includes(songList[songCount])) {
+            audio.setAttribute("src", song.data);
+          }
+        });
+        songNameContainer.innerHTML = "";
+        marquee.textContent = songList[songCount];
+        songNameContainer.append(marquee);
+        playAndPause();
       });
-    playBtn.innerHTML = `<i class="fa fa-pause" aria-hidden="true"></i>`;
-    play = true;
   }
-  // adding volume functionality
+  // Adding volume functionality
   const volumeBar = document.querySelector("#volume");
+  audio.volume = 0.3;
   volumeBar.oninput = (e) => {
     let volumeNum = eval(volumeBar.value / 100);
     audio.volume = volumeNum;
@@ -78,7 +159,7 @@
       "%)";
     volumeBar.style.background = color;
   };
-  //   adding progressBar functionality
+  // Adding progressBar functionality
   const progressBar = document.querySelector("#progress-bar");
   progressBar.oninput = () => {
     x = progressBar.value;
@@ -107,10 +188,13 @@
     progressBar.oninput = () => {
       audio.currentTime = progressBar.value;
     };
+    if (currentTime == duration) {
+      nextSong();
+    }
   });
   // Creating visualizer
   const context = new AudioContext();
-  const analyserNode = new AnalyserNode(context, { fftSize: 128 });
+  const analyserNode = new AnalyserNode(context, { fftSize: 64 });
   setupContext();
   drawAnimation();
   resize();
@@ -132,9 +216,9 @@
     const canvasContext = visualizer.getContext("2d");
     canvasContext.clearRect(0, 0, width, height);
     dataArry.forEach((item, index) => {
-      const y = ((item / 255) * height) / 1.5;
+      const y = ((item / 255) * height) / 1.2;
       const x = barWidth * index;
-      canvasContext.fillStyle = `#4BB543`;
+      canvasContext.fillStyle = `hsl(${index * 8},100%,50%)`;
       canvasContext.fillRect(x, height - y, barWidth, y);
     });
   }
@@ -142,4 +226,51 @@
     visualizer.width = visualizer.clientWidth * window.devicePixelRatio;
     visualizer.height = visualizer.clientHeight * window.devicePixelRatio;
   }
+  // Creating filereaders
+  const inpFile = document.querySelector("#song-file");
+  inpFile.addEventListener("change", async () => {
+    let db = new Localbase("db");
+    db.collection("songs")
+      .get()
+      .then(async (song) => {
+        if (song.length == 0) {
+          await db.collection("songs").add({
+            name: "yes",
+            data: "no",
+          });
+        }
+        let songName = await db
+          .collection("songs")
+          .doc({ name: inpFile.files[0].name })
+          .get();
+        if (songName) {
+          audio.setAttribute("src", songName.data);
+          marquee.textContent = inpFile.files[0].name;
+          inpFile.value = null;
+          playAndPause();
+          return;
+        } else {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(inpFile.files[0]);
+          fileReader.onload = (e) => {
+            audio.setAttribute("src", e.currentTarget.result);
+            songList.push(inpFile.files[0].name)
+            playAndPause();
+            db.collection("songs").add({
+              name: inpFile.files[0].name,
+              data: e.currentTarget.result,
+            });
+            let screen = document.querySelector("#song-option")
+            let ul = document.createElement("ul")
+            screen.innerHTML = ''
+            screen.append(ul)
+            songList.forEach(song => {
+              let li = document.createElement("li")
+              li.textContent = song
+              ul.append(li)
+            });
+          };
+        }
+      });
+  });
 })();
